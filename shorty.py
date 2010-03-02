10112b021672b0b14cb754b11e6a89946f882b07
Aleksandar Radulovic <alex@mac-alex.lan>
2010-03-02 20:54:14 +0000

    Initial commit of shorty.py

---
+++ shorty.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import bobo
+import webob
+from random import  sample
+from string import digits, ascii_letters
+from beaker.cache import CacheManager
+from beaker.util import parse_cache_config_options
+
+cache_opts = {
+    'cache.type': 'file',
+    'cache.data_dir': '/tmp/cache/data',
+    'cache.lock_dir': '/tmp/cache/lock'
+}
+
+cache = CacheManager(**parse_cache_config_options(cache_opts))
+tcache = cache.get_cache("stuff", type='dbm', expire=3600)
+
+def short_id(num): return "".join(sample(digits + ascii_letters, num))
+
+@bobo.post('/')
+def post(url):
+    id = short_id(5)
+    tcache.put(id, url)
+    return id
+
+@bobo.query('/:short', method="GET")
+def query(short):
+    if tcache.has_key(short):
+        return bobo.redirect(str(tcache.get_value(short)))
+    else:
+        return webob.Response("not found, sorry", status=404)
+
+# handler = bobo.Application(bobo_resources=__name__)
