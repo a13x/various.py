0c7a7d7779eb9354a2e3b3010b5c27ffd062dc7c
Aleksandar Radulovic <alex@a13x.info>
2011-08-29 22:23:38 +0000

    Added asyncsrv skeleton and the ini file for shorty

---
+++ asyncsrv.py
@@ -0,0 +1,32 @@
+import asyncore
+import socket
+
+class Handler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(8192)
+        if data:
+            self.send(data)
+        self.close()
+        print('processing after close')
+
+class SimpleServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
+
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is None:
+            pass
+        else:
+            sock, addr = pair
+            print('Incoming connection from %s' % repr(addr))
+            handler = Handler(sock)
+
+server = SimpleServer('localhost', 8080)
+asyncore.loop()
